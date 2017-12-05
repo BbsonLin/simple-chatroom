4db1dafb5c0fd59dcb76ce611bdf0c005d9b4bb8
Bobson Lin <bobson801104@gmail.com>
2017-12-05 19:56:50 +0800

    Modify backend to allow using socket.io by integrate Flask-SocketIO extension

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
 
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, disconnect
 
 app = Flask(__name__)
 app.config['SECRET'] = 'my secret'
 socketio = SocketIO(app)
 
+
+@socketio.on('connect')
+def on_connect():
+    print('Connect...')
+
+
+@socketio.on('disconnect')
+def on_disconnect():
+    print('Disconnect')
+    disconnect()
+
+
 @socketio.on('message')
 def handle_message(msg):
     print('Message', msg)
     send(msg, broadcast=True)
+
 
 @app.route('/')
 def index():
